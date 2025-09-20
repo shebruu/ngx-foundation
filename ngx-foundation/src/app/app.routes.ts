c6647a522af8186dffd48f460c153de90e07422c
ebru <105438469+shebruu@users.noreply.github.com>
2025-09-20 08:46:15 +0200

    config app.routes.ts home path

--- ngx-foundation/src/app/app.routes.ts
+++ ngx-foundation/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home
+  }
+];
+;
