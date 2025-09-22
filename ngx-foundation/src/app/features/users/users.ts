01c804276b86f09273a1e606f6e920028b742bfb
ebru stagiaire digitalcity <105438469+shebruu@users.noreply.github.com>
2025-09-22 11:44:30 +0200

    add userfilters

--- ngx-foundation/src/app/features/users/users.ts
+++ ngx-foundation/src/app/features/users/users.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserFilters } from '../../shared/components/user-filters/user-filters';
 
 @Component({
   selector: 'app-users',
-  imports: [RouterLink],
+  imports: [RouterLink,UserFilters],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
---
+++ ngx-foundation/src/app/shared/components/user-filters/user-filters.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-filters',
+  imports: [FormsModule],
+  templateUrl: './user-filters.html',
+  styleUrl: './user-filters.scss'
+})
+export class UserFilters {
+firstnameInput: string = '';
+
+firstnameValidation():boolean {
+  if (this.firstnameInput.length >= 3 && this.firstnameInput.length !== 0) {
+  
+    return true;
+  }
+  return false;
+}
+search():void {
+  if (this.firstnameValidation()) {
+    console.log('Searching for', this.firstnameInput);
+  }
+}
+
+}
