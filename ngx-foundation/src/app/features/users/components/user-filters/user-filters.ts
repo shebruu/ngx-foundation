c590846c3e92dab46bc5e9ee4d1987393a954c24
ebru stagiaire digitalcity <105438469+shebruu@users.noreply.github.com>
2025-09-23 11:51:18 +0200

    add user formfactory

--- ngx-foundation/src/app/features/users/components/user-filters/user-filters.ts
+++ ngx-foundation/src/app/features/users/components/user-filters/user-filters.ts
@@ -1,38 +1,37 @@
 
 import { Component, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { createUserFilterform } from '../../form/userfilter.form';
-
+import { UserFilterFormFactory } from '../../form/userfilter.form.factory';
+import { UserFilter } from '../../form/interfaces/userfilter.form';
+import {JsonPipe} from '@angular/common';
 @Component({
   selector: 'app-user-filters',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,JsonPipe],
   templateUrl: './user-filters.html',
   styleUrl: './user-filters.scss'
 })
 
 
 export class UserFilters {
+    private userFilterFormFactory = inject(UserFilterFormFactory)
+    public userFilterForm = this.userFilterFormFactory.createForm()
 
-private readonly fb = inject(NonNullableFormBuilder);
-public userfilterForm = createUserFilterform(this.fb);
-public submitForm(){
+    //Pour permettre de visualiser les valeurs contenues dans le form
+    public formContent: UserFilter | undefined;
 
-  console.log(this.userfilterForm.value);
-  if(this.userfilterForm.valid){
-    alert("formulaire bien valide");
-
-
-  }else{
-    this.userfilterForm.markAllAsTouched();
-    alert("formulaire invalide");
+    public submitForm(){
+      if(this.userFilterForm.valid){
+        this.formContent = this.userFilterForm.getRawValue();
+      } else {
+        this.userFilterForm.markAllAsTouched()
+      }
+    }
 
 }
-}
-
 
 // constructor(fb: NonNullableFormBuilder) { si pas d injection 
 //   this.fb = fb;
 // }
 
-}
+
 // injection FormsModule ou factory a la creation de composant  ()
