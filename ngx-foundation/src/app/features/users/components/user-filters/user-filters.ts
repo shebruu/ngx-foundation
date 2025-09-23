9fdf2c392634dc2e9413d0d8865c43216689724d
ebru stagiaire digitalcity <105438469+shebruu@users.noreply.github.com>
2025-09-23 13:14:15 +0200

    upate user-filters

--- ngx-foundation/src/app/features/users/components/user-filters/user-filters.ts
+++ ngx-foundation/src/app/features/users/components/user-filters/user-filters.ts
@@ -1,27 +1,33 @@
 
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { UserFilterFormFactory } from '../../form/userfilter.form.factory';
 import { UserFilter } from '../../form/interfaces/userfilter.form';
-import {JsonPipe} from '@angular/common';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-user-filters',
-  imports: [ReactiveFormsModule,JsonPipe],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-filters.html',
   styleUrl: './user-filters.scss'
 })
 
 
-export class UserFilters {
-    private userFilterFormFactory = inject(UserFilterFormFactory)
-    public userFilterForm = this.userFilterFormFactory.createForm()
 
+export class UserFilters {
+  private userFilterFormFactory = inject(UserFilterFormFactory)
+  public userFilterForm = this.userFilterFormFactory.createForm()
+  
+  //Ouput va permettre de sortir les données du formulaire en dehors du composant
+     //EventEmitter permet d'emettre l'évênement
+     @Output() FormFilterChanged = new EventEmitter<UserFilter>()
     //Pour permettre de visualiser les valeurs contenues dans le form
     public formContent: UserFilter | undefined;
 
     public submitForm(){
       if(this.userFilterForm.valid){
         this.formContent = this.userFilterForm.getRawValue();
+     console.log(this.formContent)
       } else {
         this.userFilterForm.markAllAsTouched()
       }
@@ -32,6 +38,3 @@ export class UserFilters {
 // constructor(fb: NonNullableFormBuilder) { si pas d injection 
 //   this.fb = fb;
 // }
-
-
-// injection FormsModule ou factory a la creation de composant  ()
